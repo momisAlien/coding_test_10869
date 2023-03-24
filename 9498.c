68cf3a32f090a5825e5929e08076298cb867f933
MomisAlien <127112536+momisAlien@users.noreply.github.com>
2023-03-24 13:54:47 +0900

    Add files via upload

---
+++ 9498.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int score = 0; {
+		scanf_s("%d", &score);
+		if (89 < score)
+			printf("A");
+		else if (79 < score)
+		printf("B");
+		else if (69 < score)
+		printf("C");
+		else if (59 < score)
+			printf("D");
+
+		else printf("F");
+	}
+
+
+	return 0;
+
+}
