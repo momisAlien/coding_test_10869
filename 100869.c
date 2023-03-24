cc270fab9164a097132aaa02259944e44e6e5c7a
MomisAlien <127112536+momisAlien@users.noreply.github.com>
2023-03-24 13:20:04 +0900

    Update and rename 10039.c to 100869.c

---
+++ 100869.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int num_1 = 0, num_2 = 0;
+	{
+		scanf_s("%d %d", &num_1, &num_2);
+		printf("%d\n", num_1 + num_2);
+
+		printf("%d\n", num_1 - num_2);
+
+		printf("%d\n", num_1 * num_2);
+	
+		printf("%d\n", num_1 / num_2);
+	
+		printf("%d\n", num_1 % num_2);
+
+		return 0; }
+		
+
+
+
+
+}
